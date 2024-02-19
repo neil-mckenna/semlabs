868974c1dc55f5d80de20863289c3ceae71d588a
neil-mckenna <neil.mckenna.dev@gmail.com>
2024-02-19 12:11:51 +0000

    Updated Employee class added database and updated the main app to run off that

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -10,7 +10,17 @@ public class App {
         // Connect to database
         a.connect();
 
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+
+        // Display results
+        a.displayEmployee(emp);
+
+
         // Disconnect from database
         a.disconnect();
     }
+
+
+
 }
